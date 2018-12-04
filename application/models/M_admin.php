2d7a531e2ffeb3302e972df4d4f3ddd18ac817fe
L200160116 <iqbalnur833@gmail.com>
2018-12-04 10:09:51 +0700

    Initial commit

---
+++ application/models/M_admin.php
@@ -0,0 +1,46 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class M_admin extends CI_Model{
+
+  public function __construct()
+  {
+    parent::__construct();
+    //Codeigniter : Write Less Do More
+  }
+  function loadProfile($id){
+    $sql = "SELECT * FROM admin WHERE id_admin='$id'";
+    return $this->db->query($sql)->result();
+  }
+
+
+  function dataBarang(){
+    $sql = "SELECT * FROM barang";
+    return $this->db->query($sql)->result();
+  }
+
+  function barangPinjaman(){
+    $sql = "SELECT * FROM barang JOIN peminjam_has_barang WHERE barang.no_barang=peminjam_has_barang.no_barang";
+    return $this->db->query($sql)->result();
+  }
+
+  // -------------------------  Barang -----------------
+  function insertBarang($data){
+    return $this->db->insert('barang', $data);
+  }
+
+  function deleteBarang($data){
+    return $this->db->delete('barang', $data);
+  }
+
+  function editBarang($data){
+    $sql = "SELECT * FROM barang WHERE no_barang='$data'";
+    return $this->db->query($sql)->result();
+  }
+
+  // ------------------------ Pemberitahuan -----------------------------
+  // function loadPemberitahuan(){
+  //   $sql = "SELECT * FROM "
+  // }
+
+}
